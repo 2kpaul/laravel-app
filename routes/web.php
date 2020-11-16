cdbee493ec363dd7782df762a2704e167c8f205d
Paul Maxineanu <paul.maxineanu@gmail.com>
2020-11-16 17:34:11 +0200

    add resources models, controllers, migrations and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'pages' => PageController::class,
+    'articles' => ArticleController::class,
+    'categories' => CategoryController::class,
+    'tags' => TagController::class,
+]);
